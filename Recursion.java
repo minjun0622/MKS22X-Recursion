414587e7778772f1cd48facf6958bc848a038da7
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-01-31 22:44:45 -0500

    Making changes and filled out skeleton methods.

---
+++ Recursion.java
@@ -0,0 +1,33 @@
+
+public class Recursion{
+    /*You may write additional private methods */
+    /*Recursively find the sqrt using Newton's approximation
+     *tolerance is the allowed percent error the squared answer is away from n.
+     *precondition: n is non-negative
+    */
+    public static double sqrt(double n, double tolerance){
+      if ((n - tolerance) < 0.0001) {
+        return n;
+      }
+      else {
+
+      }
+
+    }
+
+    /*Recursively find the n'th fibbonaci number in linear time
+     *fib(0) = 1; fib(1) = 1; fib(5) = 5
+     *precondition: n is non-negative
+     */
+    public static int fib(int n){
+
+    }
+
+    /*As Per classwork*/
+    public static ArrayList<Integer> makeAllSums(){
+      sumDigits()
+    }
+
+    private static int sumDigits()
+
+}
