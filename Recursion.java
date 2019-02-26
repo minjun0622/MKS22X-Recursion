f5c4c9f897352334c506b4a5e118af5d3c53025c
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-02-25 19:34:17 -0500

    Added the driver and fixed mysqrt.

--- Recursion.java
+++ Recursion.java
@@ -1,65 +1,5 @@
 import java.util.ArrayList;
 public class Recursion{
-  public static void main(String[] args) {
-        System.out.println("sqrt()");
-        System.out.println("-----------------------\n");
-
-        System.out.println("sqrt(4, 0.00001)");
-        System.out.println("EXPECTED: 2");
-        System.out.println(sqrt(4, 0.00001));         // 2
-
-        System.out.println("----");
-
-        System.out.println("sqrt(8, 0.00001)");
-        System.out.println("EXPECTED: ~2.8284");
-        System.out.println(sqrt(8, 0.00001));         // ~2.8284
-
-        System.out.println("----");
-
-        System.out.println("sqrt(0, 0.00001)");
-        System.out.println("EXPECTED: 0");
-        System.out.println(sqrt(0, 0.00001));         // 0
-
-        System.out.println("\n");
-
-        System.out.println("fib()");
-        System.out.println("-----------------------\n");
-
-        System.out.println("fib(0)");
-        System.out.println("EXPECTED: 0");
-        System.out.println(fib(0));                   // 0
-
-        System.out.println("----");
-
-        System.out.println("fib(1)");
-        System.out.println("EXPECTED: 1");
-        System.out.println(fib(1));                   // 1
-
-        System.out.println("----");
-
-        System.out.println("fib(7)");
-        System.out.println("EXPECTED: 13");
-        System.out.println(fib(7));                   // 13
-
-        System.out.println("makeAllSums()");
-        System.out.println("-----------------------\n");
-
-        System.out.println("makeAllSums(0)");
-        System.out.println("EXPECTED: [0]");
-        System.out.println(makeAllSums(0));           // [0]
-
-        System.out.println("----");
-
-        System.out.println("makeAllSums(1)");
-        System.out.println("EXPECTED: [1, 0]");
-        System.out.println(makeAllSums(1));           // [1, 0]
-
-        System.out.println("----");
-
-        System.out.println("makeAllSums(3)");
-        System.out.println("EXPECTED: [0, 3, 2, 5, 1, 4, 3, 6]");
-        System.out.println(makeAllSums(3));           // [0, 3, 2, 5, 1, 4, 3, 6]
-}
     /*You may write additional private methods */
     /*Recursively find the sqrt using Newton's approximation
      *tolerance is the allowed percent error the squared answer is away from n.
@@ -74,13 +14,13 @@ public class Recursion{
         return 0;
       }
       else {
-        return mysqrt(n, tolerance, 1);
+        return mysqrt(n, tolerance, n/2);
       }
     }
 
     //this helper finds the guess if it is not equal to the permitted value.
     private static double mysqrt(double n, double tolerance, double guess) {
-      if (Math.abs((guess * guess - n) / (n * 1000)) < tolerance) {
+      if (Math.abs(guess * guess - n) <= tolerance) {
         return guess;
     }
     else {
